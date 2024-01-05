ae0beabf29ede7f090672a47f44aa1cdc4f3f75e
baka4n <qq474899581@gmail.com>
2024-01-05 15:46:09 +0800

    更新apply

--- core/src/main/java/io/github/xenfork/acl/MainAware.java
+++ core/src/main/java/io/github/xenfork/acl/MainAware.java
@@ -4,16 +4,35 @@ import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.initialization.Settings;
 import org.gradle.api.plugins.PluginAware;
+import org.gradle.api.plugins.PluginContainer;
 import org.jetbrains.annotations.NotNull;
 
 public class MainAware implements Plugin<PluginAware> {
     @Override
     public void apply(@NotNull PluginAware target) {
        if (target instanceof Settings settings) {
+           apply(settings.getPlugins(), MainSettings.class);
            new MainSettings().apply(settings);
-       } else if (target instanceof Project) {
-           target.getPlugins().apply("java");
-           target.getPlugins().apply("maven-publish");
+       } else if (target instanceof Project project) {
+           apply(
+                   project.getPlugins(),
+                   "java",
+                   "maven-publish"
+           );
+           apply(project.getPlugins(), Main.class);
        }
+    }
+
+    @SafeVarargs
+    public static void apply(PluginContainer plugins, Class<? extends Plugin<?>>... p) {
+        for (Class<? extends Plugin<?>> aClass : p) {
+            plugins.apply(aClass);
+        }
+    }
+
+    public static void apply(PluginContainer plugins, String... strings) {
+        for (String string : strings) {
+            plugins.apply(string);
+        }
     }
 }
